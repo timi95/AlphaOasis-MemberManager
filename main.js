a0c51d182fe12425e80d9cbeb6e757cbc5a8bcc9
timi95 <timi.ogunkeye@gmail.com>
2019-02-27 07:37:18 +0100

    Electron wrap working

--- main.js
+++ main.js
@@ -2,21 +2,42 @@ const { app, BrowserWindow } = require('electron')
 
 let win;
 
-function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600, icon: `file://{__dirname}/dist/assets/favicon.ico` })
-    win.loadURL(`file://{__dirname}/dist/index.html`);
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 600, 
+    height: 600,
+    //backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/alpha-oasis/assets/favicon.ico`
+  })
 
-    win.on('closed', function(){ wind = null})
 
-    app.on('window-all-closed', function(){
-        if (proccess.platform !== 'darwin') {
-            app.quit()
-        }
-    })
+  win.loadURL(`file://${__dirname}/dist/alpha-oasis/index.html`)
 
-    app.on('activate', function() {
-        if (win === null) {
-            createWindow()
-        }
-    })
+  //// uncomment below to open the DevTools.
+  win.webContents.openDevTools()
+
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
 }
+
+// Create window on electron intialization
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
