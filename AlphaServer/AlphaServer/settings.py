ecd00de0552373a5c7d9b6a047f878aab44ad02f
timi95 <timi.ogunkeye@gmail.com>
2019-03-02 21:48:01 +0100

    back to sqlite

--- AlphaServer/AlphaServer/settings.py
+++ AlphaServer/AlphaServer/settings.py
@@ -94,11 +94,19 @@ WSGI_APPLICATION = 'AlphaServer.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-   'default' : {
-      'ENGINE' : 'djongo',
-      'NAME' : 'account_database',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+#MongoDB option
+# DATABASES = {
+#    'default' : {
+#       'ENGINE' : 'djongo',
+#       'NAME' : 'account_database',
+#    }
+# }
 
 
 # Password validation
--- AlphaServer/AlphaServer/wsgi.py
+++ AlphaServer/AlphaServer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AlphaServer.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AlphaServer.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'AlphaServer.settings'
 application = get_wsgi_application()
